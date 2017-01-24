5a36a197a0444116e8daeafce464c2ed3459c8f4
Mitchell Hewes <me@mitcdh.com>
2017-01-24 11:25:06 +1100

    Wrapped caddyfile.head cat in an if statement

--- files/run.sh
+++ files/run.sh
@@ -4,8 +4,12 @@
 run-parts --exit-on-error /caddy-bootstrap/pre-run/ || exit $?
 
 # generate our caddyfile from provided head
+if [ -e "/caddy-bootstrap/Caddyfile" ]; then
+	echo "config check: found head caddyfile"
+	cat /caddy-bootstrap/Caddyfile > /www/Caddyfile
+fi
+
 IFS=$'\n'
-cat /caddy-bootstrap/Caddyfile > /www/Caddyfile
 echo "$*" >> /www/Caddyfile
 
 # exec caddy as www-data from a clean login shell
